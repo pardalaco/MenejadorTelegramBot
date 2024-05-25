77d4f485f5c732d1aeeffa598a033381c6f4d669
Daniel Rovira Martínez <pardalaco@gmail.com>
2024-05-25 12:44:19 +0200

    imgDownloader

--- imgDownloader.py
+++ imgDownloader.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import time
 import numpy as np
 import cv2
+import yolo_object_detector.yolo_detector_function as yl
 
 
 def descargar_imagen(url):
@@ -35,3 +36,55 @@ def imagen_to_cv2(content):
         return imagen
     except Exception as e:
         print("Ocurrió un error al procesar la imagen:", str(e))
+
+def guardar_imagen_cv2(imagen_cv2, carpeta_base='./img/'):
+    # Obtener la fecha actual
+    fecha_actual = datetime.now().strftime("%Y-%m-%d")
+    
+    # Crear la carpeta con la fecha actual
+    carpeta_guardado = carpeta_base + fecha_actual
+    
+    # Crear la carpeta si no existe
+    if not os.path.exists(carpeta_guardado):
+        os.makedirs(carpeta_guardado)
+    
+    # Obtener la hora actual
+    hora_actual = datetime.now().strftime("%H-%M")
+    
+    # Crear el nombre del archivo con la hora actual
+    nombre_archivo = f"{fecha_actual+"_"+hora_actual}.jpg"
+    
+    # Crear el nombre completo del archivo con la ruta de la carpeta
+    nombre_archivo_completo = os.path.join(carpeta_guardado, nombre_archivo)
+    
+    # Guardar la imagen en formato JPG
+    cv2.imwrite(nombre_archivo_completo, imagen_cv2)
+        
+
+
+while True:  # Bucle infinito para ejecutar continuamente
+    # Cargar la imagen con OpenCV
+    image = descargar_imagen('https://www.acifalcoi.com/webcam/menejador.jpg')
+    image = imagen_to_cv2(image)
+
+
+    if(image is None):
+        time.sleep(60)
+        pass
+
+    fecha_descarga = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Imagen descargada ({fecha_descarga}).", end="\r")
+
+    image_yl = yl.detect_objects(image)
+    image = image_yl[0]
+    labels = image_yl[1]
+
+    # Verificar si hay algo en labels antes de enviar el mensaje
+    if 'person' in labels:
+        guardar_imagen_cv2(image)
+        print()
+        print(f"Imagen guardada ({fecha_descarga}).")
+
+
+    # Esperar un tiempo antes de la siguiente iteración para evitar sobrecargar el sistema
+    time.sleep(60)  # Esperar 1 minuto antes de la próxima ejecución
